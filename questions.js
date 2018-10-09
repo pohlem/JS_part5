ab3b6e38b0fca6955e23cb00da9ba05a819056d9
pohlem <pohle.maxime@gmail.com>
2018-10-09 14:48:12 +0200

    Init commit

---
+++ questions.js
@@ -0,0 +1,55 @@
+var languagesArrayCreation = function () {
+
+  return 'A compléter';
+}
+
+var numbersArrayCreation = function () {
+
+    return 'A compléter';
+}
+
+var ElementReplacement = function (languages) {
+
+  return 'A compléter';
+}
+
+var AddElementToLanguagesArray = function (languages) {
+
+  return 'A compléter';
+}
+
+var AddElementToNumbersArray = function (numbers) {
+
+  return 'A compléter';
+}
+
+var deleteArrayFirstElement = function (languages) {
+
+  return 'A compléter';
+}
+
+var deleteArrayLastElement = function (languages) {
+
+  return 'A compléter';
+}
+
+var stringToArray = function (socialMediaInString) {
+
+  return 'A compléter';
+}
+
+var arrayToString = function (languages) {
+
+  return 'A compléter';
+}
+
+var arraySort = function (socialMedia) {
+
+  return 'A compléter';
+}
+
+var arrayInvert = function (languages){
+
+
+  return 'A compléter';
+}
